c760b020b67136ead400ff4eead7de9a857d77ce
Amir Iskandar <amiriskandar@graduate.utm.my>
2022-11-30 00:34:49 +0800

    update sprint 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin as MiddlewareAdmin;
+use Database\Seeders\Admin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => Admin::class
+        'admin' => \App\Http\Middleware\Admin::class
     ];
 }
